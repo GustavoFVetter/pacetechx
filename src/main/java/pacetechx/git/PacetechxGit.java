64a3b248e554d7e88053eef92291e780fed31c06
Gustavo F. Vetter <vetter.gustavo@gmail.com>
2022-08-16 13:32:22 -0300

    Meu primeiro commit

---
+++ src/main/java/pacetechx/git/PacetechxGit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pacetechx.git;
+
+/**
+ *
+ * @author Gustavo F. Vetter, em 16/08/22
+ */
+public class PacetechxGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Heloo world!!!");
+    }
+    
+}
